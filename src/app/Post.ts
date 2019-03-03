70a2c3d13a73a77d981be3444f53d4cc488521d0
speedy1981 <maoudi.mazouz@gmail.com>
2019-03-03 23:09:46 +0100

    delete console.log and change value loveit

--- src/app/Post.ts
+++ src/app/Post.ts
@@ -4,7 +4,6 @@ export class Post {
   loveIts: number;
   created_at: Date;
   constructor(_title:string,_content:string,_loveIts:number){
-      console.log(_title);
       this.title=_title;
       this.content=_content;
       this.created_at=new Date();
