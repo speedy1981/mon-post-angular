eb4dab10f669342bbb335e44ce76963f0e6d50a5
speedy1981 <maoudi.mazouz@gmail.com>
2019-03-04 10:31:32 +0100

    post with 0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,10 @@ import {PostListComponent} from './post-list/post-list.component';
 })
 export class AppComponent {
    // initialisation des postes
-    post1=new Post('mon premier post','Bonjour, je cherche un cours angular2, enfin je suis tombé sur le cours que cherchait depuis des mois',5);
-    post2=new Post('mon deuxième post','Bonjour, je cherche un cours angular2, enfin je suis tombé sur le cours que cherchait depuis des mois',-1);
-    post3=new Post('mon troisième post','Bonjour, je cherche un cours angular2, enfin je suis tombé sur le cours que cherchait depuis des mois',2);
-    post4=new Post('mon quatrième post','Bonjour, je cherche un cours angular2, enfin je suis tombé sur le cours que cherchait depuis des mois',3);
+    post1=new Post('mon premier post','Bonjour, je cherche un cours angular2, enfin je suis tombé sur le cours que cherchait depuis des mois',0);
+    post2=new Post('mon deuxième post','Bonjour, je cherche un cours angular2, enfin je suis tombé sur le cours que cherchait depuis des mois',0);
+    post3=new Post('mon troisième post','Bonjour, je cherche un cours angular2, enfin je suis tombé sur le cours que cherchait depuis des mois',0);
+    post4=new Post('mon quatrième post','Bonjour, je cherche un cours angular2, enfin je suis tombé sur le cours que cherchait depuis des mois',0);
 
  posts:Array<Post>=[this.post1,this.post2,this.post3,this.post4];
  
